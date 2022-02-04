3b0c2ea28650dd207abd8329b4d71c82c0f86dcb
Ryan Vanny <73187503+Obarquinho@users.noreply.github.com>
2022-02-03 22:07:37 -0800

    Update subtracter.java

--- subtracter.java
+++ subtracter.java
@@ -1,5 +1,5 @@
-class subtracter{
+public class subtracter{
     int start;
     public subtracter(int input){this.start=input;}
-    public int sub(int input){return this.start+=input;}
+    public int sub(int input){return this.start-=input;}
 }
